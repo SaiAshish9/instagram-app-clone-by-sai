77af2b1bf489591f9676a60a89b716f60da423cb
saiashish9 <saiashish3760@gmail.com>
2021-01-25 19:53:42 +0530

    initial commit

---
+++ App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+import {StyleSheet, View, Text} from 'react-native';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text>Instagram</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+});
+
+export default App;
---
+++ README.md
@@ -0,0 +1,12 @@
+```
+Configuration Step's :
+
+export ANDROID_HOME=$HOME/Android/Sdk
+export PATH=$PATH:$ANDROID_HOME/tools
+
+OR
+
+local.properties -> ANDROID_SDK_ROOT = /home/saiashish9/Android/Sdk
+
+Android Studio -> tools -> Sdk Manager -> Version 29
+```
